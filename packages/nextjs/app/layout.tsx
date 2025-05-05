476fe04feb3b0b2271a527056466f677a3da771c
0xTrip <charliefreeman@live.co.uk>
2025-05-05 21:15:49 +0100

    visual changes on nextjs

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "Scaffold-kadena App",
+  description: "Built with 🏗 Scaffold-kadena",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
