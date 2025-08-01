8dbf1839b58c9b17fca2d4d9826b942bf84c502b
Heather S <42716568+hswopeams@users.noreply.github.com>
2025-08-01 14:50:15 +0200

    Change hardhat config to correct chainweb plugin config

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -18,70 +18,16 @@ const config: HardhatUserConfig = {
     },
   },
 
-  defaultNetwork: "hardhat",
-  defaultChainweb: "testnet",
-
-  networks: {
-    hardhat: { chainId: 31337 },
-
-    // Testnet networks
-    testnet20: {
-      chainId: 5920,
-      url: "https://evm-testnet.chainweb.com/chainweb/0.0/evm-testnet/chain/20/evm/rpc",
-      accounts,
-    },
-    testnet21: {
-      chainId: 5921,
-      url: "https://evm-testnet.chainweb.com/chainweb/0.0/evm-testnet/chain/21/evm/rpc",
-      accounts,
-    },
-    testnet22: {
-      chainId: 5922,
-      url: "https://evm-testnet.chainweb.com/chainweb/0.0/evm-testnet/chain/22/evm/rpc",
-      accounts,
-    },
-    testnet23: {
-      chainId: 5923,
-      url: "https://evm-testnet.chainweb.com/chainweb/0.0/evm-testnet/chain/23/evm/rpc",
-      accounts,
-    },
-    testnet24: {
-      chainId: 5924,
-      url: "https://evm-testnet.chainweb.com/chainweb/0.0/evm-testnet/chain/24/evm/rpc",
-      accounts,
-    },
-
-    // Sandbox networks
-    sandbox20: { chainId: 1789, url: "http://localhost:1848/chainweb/0.0/evm-development/chain/20/evm/rpc", accounts },
-    sandbox21: { chainId: 1790, url: "http://localhost:1848/chainweb/0.0/evm-development/chain/21/evm/rpc", accounts },
-    sandbox22: { chainId: 1791, url: "http://localhost:1848/chainweb/0.0/evm-development/chain/22/evm/rpc", accounts },
-    sandbox23: { chainId: 1792, url: "http://localhost:1848/chainweb/0.0/evm-development/chain/23/evm/rpc", accounts },
-    sandbox24: { chainId: 1793, url: "http://localhost:1848/chainweb/0.0/evm-development/chain/24/evm/rpc", accounts },
-  },
-
   namedAccounts: {
     deployer: { default: 0 },
   },
 
   chainweb: {
     hardhat: { chains: 5 },
-    sandbox: {
-      type: "external",
-      chains: 5,
-      accounts,
-      chainIdOffset: 1789,
-      chainwebChainIdOffset: 20,
-      externalHostUrl: "http://localhost:1848/chainweb/0.0/evm-development",
-      etherscan: {
-        apiKey: "abc",
-        apiURLTemplate: "http://chain-{cid}.evm.kadena.local:8000/api/",
-        browserURLTemplate: "http://chain-{cid}.evm.kadena.local:8000/",
-      },
-    },
     testnet: {
       type: "external",
       chains: 5,
-      accounts,
+      accounts: [accounts],
       chainIdOffset: 5920,
       chainwebChainIdOffset: 20,
       externalHostUrl: "https://evm-testnet.chainweb.com/chainweb/0.0/evm-testnet",
