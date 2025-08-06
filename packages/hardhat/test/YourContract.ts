4223c00ccdbe703767b548f5719f3bd0485208d0
Heather S <42716568+hswopeams@users.noreply.github.com>
2025-08-06 11:32:37 +0200

    Fix Should have the right message on deploy test case

--- packages/hardhat/test/YourContract.ts
+++ packages/hardhat/test/YourContract.ts
@@ -15,7 +15,7 @@ describe("YourContract", function () {
 
   describe("Deployment", function () {
     it("Should have the right message on deploy", async function () {
-      expect(await yourContract.greeting()).to.equal("Building Unstoppable Apps!!!");
+      expect(await yourContract.greeting()).to.equal("Build on Kadena!!!");
     });
 
     it("Should allow setting a new message", async function () {
